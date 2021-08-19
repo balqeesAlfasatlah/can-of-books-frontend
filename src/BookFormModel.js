4be892d1f666a3b0a1fb713bfd83d733b2f4c4da
balqessAlfasatlah <BalqeesAlfasatlah@yahoo.com>
2021-08-19 10:04:03 +0300

    done

--- src/BookFormModel.js
+++ src/BookFormModel.js
@@ -1,30 +1,60 @@
 import React from 'react';
-import  {MOdal , Button, Form} from 'react-bootstrap';
-
-
+import {Button, Form } from 'react-bootstrap';
 import React, { Component } from 'react'
 import { Modal } from 'bootstrap';
 
 export class BookFormModel extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
     }
     render() {
         return (
-           <Modal show={this.props.showModal} onHide={this.props.closeHandler}>
-           <Modal.Header closeButton>
-               <Modal.Title>Add your book</Modal.Title>
-           </Modal.Header>
-           <Modal.Body>    
-               <Form onSubmit={this.props.addBook}
-               >
-           <Form.Label></Form.Label> 
-           <Form.Control size="lg" type="text" placeholder="book title" name="addTitle"/>
-           <Form.Control size="lg" type="text" placeholder="book description" name="addDescription"/>
-           <Form.Control size="lg" type="text" placeholder="book img" name="addImg"/>
-           <Form.Control size="lg" type="text" placeholder="book status" name="addStatus"/>
-           <input type="submkit" value="Submit"/>
+
+            <Modal show={this.props.showModal} onHide={this.props.closeHandler}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add your book</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+
+                    <Form onSubmit={this.props.addBook}>
+
+                        <Form.Control size="lg" type="text" placeholder="book title" name="addTitle" />
+                        <Form.Control size="lg" type="text" placeholder="book description" name="addDescription" />
+                        <Form.Control size="lg" type="text" placeholder="book img" name="addImg" />
+                        <Form.Control size="lg" type="text" placeholder="book status" name="addStatus" />
+                        <input type="submit" value="Submit" />
+                    </Form>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+
+                </Modal.Footer>
+            </Modal>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         )
     }
--- src/MyFavoriteBooks.js
+++ src/MyFavoriteBooks.js
@@ -8,43 +8,43 @@ import UpdateForm from './UpdateForm';
 
 class MyFavoriteBooks extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      userData:[],
-      showModal :false
+      userData: [],
+      showModal: false
     }
   }
 
-   handleModel =()=>{
-     this.setState({
-       showModal : true
-     })
-   }
-
-   handleClose = () =>{
+  handleModel = () => {
     this.setState({
-      showModal : false
+      showModal: true
     })
-   }
+  }
 
-   componentDidMount= async ()=>{
-     axios.get(
+  handleClose = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  componentDidMount = async () => {
+    axios.get(
       `http://localhost:8080/books?email=${this.props.auth0.user.email}`
     )
-    .then(response => 
-      this.setState({
-      userData : response.data.books
-    })
-   
-    ) 
+      .then(response =>
+        this.setState({
+          userData: response.data.books
+        })
+
+      )
   }
 
 
 
-  addBook = async (e)=>{
+  addBook = async (e) => {
     e.preventDefault();
-    const {user} = this.props.auth0;
+    const { user } = this.props.auth0;
     let addTitle = e.target.addTitle.value;
     let addDescription = e.target.addDescription.value;
     let addImg = e.target.addImg.value;
@@ -53,45 +53,70 @@ class MyFavoriteBooks extends React.Component {
   }
 
 
-   const addBookForm ={
-    addTitle = e.target.addTitle.value;
-     addDescription = e.target.addDescription.value;
-     addImg = e.target.addImg.value;
-     addStatus = e.target.addStatus.value;
-     email = user.email
+  addBookForm = {
+    addTitle = e.target.addTitle.value,
+    addDescription = e.target.addDescription.value,
+    addImg = e.target.addImg.value,
+    addStatus = e.target.addStatus.value,
+    email = user.email
+  }
+
+
+   bestBooks = await axios.post(`${process.env.REACT_APP_BOOK}/book`, addBookForm){
+    this.setState({
+      bestBooks: bestBooks.data
+    })
    }
 
 
-  let bestBooks = await axios.post(`${process.env.REACT_APP_BOOK}/book`, addBookForm)
+
+deleteBook = async (idx) => {
+  const { user } = this.props.auth0;
+}
+
+let paramsObject = {
+  email: user.email
+}
+
+
+let bestBooks = await axios.delete(`${this.state.server}/deleteBook/${idx}`, { params: paramsObject })
+this.setState({
+  bestBooks: booksData.data
+})
+
+
+UpdateForm = (index) => {
   this.setState({
-    bestBooks : bestBooks.data
+    index : index,
+    title: this.state.userData.title,
+    status: this.status.userData.status,
+    description: this.state.userData.description,
   })
 
+  
+}
 
-  deleteBook = async(idx) =>{
-    const {user} = this.props.auth0;
+
+Updates = (e) => {
+  e.preventDefault();
+  let update  ={
+    email : this.props.auth0.user.email ,
+    bookTitle : this.props.bookTitle.value ,
+    bookDescription : this.props.bookDescription.value,
+    bookStatus : this.props.bookStatus.value
   }
 
-  let paramsObject ={
-    email: user.email
-  }
-
-
-  let bestBooks = await axios.delete(`${this.state.server}/deleteBook/${idx}`,{params :paramsObject})
+  
+ axios.put(`${process.env.REACT_APP_BOOK}/book-update`, update).then(item=>{
   this.setState({
-    bestBooks : booksData.data
-  }) 
+    userData: item.data
+  })
+ })
+  
+  
 
-
-  UpdateForm = (e)=>{
-    const jwt = result;
-    let data ={
-      title :this.state.title,
-      status :this.status.status,
-      description :this.state.description,
-
-    }
-  }
+  
+}
 
 //   componentDidMount= ()=>{
 //     if (this.props.auth0.isAuthenticated){
@@ -108,49 +133,51 @@ class MyFavoriteBooks extends React.Component {
 //                 this.setState({
 //                     usersList:response.data
 //                 })
-                
+
 //             })
 //         })
 //     }
 // }
 
-  render() {
-    return(
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-        
-        <h2> { this.props.auth0.isAuthenticated && this.state.userData.map((item)=>{
-          return(
-            <>
-            <img src ={item.img} alt ={item.name}/>
+render() {
+  return (
+    <Jumbotron>
+      <h1>My Favorite Books</h1>
+      <p>
+        This is a collection of my favorite books
+      </p>
+
+      <h2> {this.props.auth0.isAuthenticated && this.state.userData.map((item) => {
+        return (
+          <>
+            <img src={item.img} alt={item.name} />
             <h2>{item.name}</h2>
             <p>{item.description}</p>
             <h3>{item.states}</h3>
-            </>
-          )
-        })}</h2>
+          </>
+        )
+      })}</h2>
 
 
-
-<h3> {this.state.bestBooks.map((book , idx)=>{
-          return(
-            <>
-            <img src ={book.img} alt ={book.title}/>
+      <button onClick ={this.addBook}>add book</button>
+      <BookFormModel addBook ={this.addBook} handleClose ={this.handleClose} showModal ={this.showModal}></BookFormModel>
+      <h3> {this.state.userData.map((book, idx) => {
+        return (
+          <>
+            <img src={book.img} alt={book.title} />
             <h2>{book.name}</h2>
             <p>{book.description}</p>
             <h3>{book.states}</h3>
-            </>
-          )
-        })}</h3>
+          </>
+        )
+      })}</h3>
 
-        <Button onClick={()=>this.deleteBook(idx)}>Delete</Button>
-        {/* {console.log(this.state.userData)} */}
-      </Jumbotron>
-    )
-  }
+      <Button onClick={() => this.deleteBook(idx)}>Delete</Button>
+      <Button onClick={() => this.UpdateForm(idx)}>Update</Button>
+      {/* {console.log(this.state.userData)} */}
+    </Jumbotron>
+  )
+
 }
 
 export default withAuth0(MyFavoriteBooks);
